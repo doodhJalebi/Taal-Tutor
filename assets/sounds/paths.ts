26b090784daf8fd65b79145b32c885b69d112db1
Owais <blackhawk2399@gmail.com>
2020-12-02 12:51:41 +0500

    Auto-generated commit

---
+++ assets/sounds/paths.ts
@@ -0,0 +1,15 @@
+export const sounds = {
+    dha: "../../{{ baseurl }}/assets/sounds/Bol/Dha.mp3",
+    dhin: "../../assets/sounds/Bol/Dhin.mp3",
+    kath: "../../assets/sounds/Bol/Kath.mp3",
+    na: "../../assets/sounds/Bol/Na.mp3",
+    tin: "../../assets/sounds/Bol/Tin.mp3",
+    tirkit: "../../assets/sounds/Bol/Tirkit.mp3",
+    tite: "../../assets/sounds/Bol/Tite.mp3",
+    dadra: "../../assets/sounds/Theka/dadra.mp3",
+    rupak: "../../assets/sounds/Theka/rupak.mp3",
+    keherwa: "../../assets/sounds/Theka/keherwa.mp3",
+    jhaptal: "../../assets/sounds/Theka/jhaptal.mp3",
+    ektal: "../../assets/sounds/Theka/ektal.mp3",
+    teentaal: "../../assets/sounds/Theka/teentaal.mp3"
+};
