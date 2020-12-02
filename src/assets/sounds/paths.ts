b3aa8c2a3b22d0b5ae91d9abca0db4fe38f88266
Owais <blackhawk2399@gmail.com>
2020-12-02 13:32:00 +0500

    Added production paths for sound files

--- src/assets/sounds/paths.ts
+++ src/assets/sounds/paths.ts
@@ -1,5 +1,5 @@
-export const sounds = {
-    dha: "/Taal-Tutor/assets/sounds/Bol/Dha.mp3",
+/* export const sounds = {
+    dha: "../../assets/sounds/Bol/Dha.mp3",
     dhin: "../../assets/sounds/Bol/Dhin.mp3",
     kath: "../../assets/sounds/Bol/Kath.mp3",
     na: "../../assets/sounds/Bol/Na.mp3",
@@ -12,4 +12,20 @@ export const sounds = {
     jhaptal: "../../assets/sounds/Theka/jhaptal.mp3",
     ektal: "../../assets/sounds/Theka/ektal.mp3",
     teentaal: "../../assets/sounds/Theka/teentaal.mp3"
+}; */
+
+export const sounds = {
+    dha: "/Taal-Tutor/assets/sounds/Bol/Dha.mp3",
+    dhin: "/Taal-Tutor/assets/sounds/Bol/Dhin.mp3",
+    kath: "/Taal-Tutor/assets/sounds/Bol/Kath.mp3",
+    na: "/Taal-Tutor/assets/sounds/Bol/Na.mp3",
+    tin: "/Taal-Tutor/assets/sounds/Bol/Tin.mp3",
+    tirkit: "/Taal-Tutor/assets/sounds/Bol/Tirkit.mp3",
+    tite: "/Taal-Tutor/assets/sounds/Bol/Tite.mp3",
+    dadra: "/Taal-Tutor/assets/sounds/Theka/dadra.mp3",
+    rupak: "/Taal-Tutor/assets/sounds/Theka/rupak.mp3",
+    keherwa: "/Taal-Tutor/assets/sounds/Theka/keherwa.mp3",
+    jhaptal: "/Taal-Tutor/assets/sounds/Theka/jhaptal.mp3",
+    ektal: "/Taal-Tutor/assets/sounds/Theka/ektal.mp3",
+    teentaal: "/Taal-Tutor/assets/sounds/Theka/teentaal.mp3"
 };
